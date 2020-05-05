d8ad587d411da189628506e61bc1757297ed3a2c
Alexander Vinogradov <alexvingra@mail.ru>
2020-05-05 19:40:59 +0300

    Fix bugs, add comments

--- bot.py
+++ bot.py
@@ -21,37 +21,35 @@ vk_session = vk_api.VkApi(
 longpoll = VkBotLongPoll(vk_session, 194275944)
 vk = vk_session.get_api()
 
-COUNTER = 0  # Счётчик номера вопроса
-CORRECT = 0  # Счётчик правильных ответов в игре
-WRONG = 0  # Счётчик неверных ответов в игре
-LEVEL = '-'
-LEARNING = False
-SIGHTS = False
 
+# {user_id: [MODE, LEVEL, [used], CORRECT, WRONG, COUNTER, CURRENT_PROMPT]}
+USERS = {}
+
+# Словарь кодов эмодзи-чисел
 EM_NUMBERS = {1: '1&#8419;', 2: '2&#8419;', 3: '3&#8419;', 4: '4&#8419;', 5: '5&#8419;',
               6: '6&#8419;', 7: '7&#8419;', 8: '8&#8419;', 9: '9&#8419;', 10: '&#128287;'}
 
 
-'''Функция возваращет изображение bytes-like объекта, вызывается в функции photo для отправки
-изображения пользователю'''
+'''
+Функция возваращет изображение bytes-like объекта и название объекта, 
+вызывается в функции photo для отправки изображения пользователю в режиме MODE = town
+'''
 
 
-def get_image(level):
+def get_image(level, user_id):
     with open('cities.txt') as fi:
         cities = fi.read().split('.')
-    if LEVEL == 'лёгкий':
+    if USERS[user_id][1] == 'лёгкий':
         cities = cities[0].split('\n')
     else:
         cities = cities[1].split('\n')
     if '' in cities:
         del cities[cities.index('')]
     city = random.sample(cities, 1)
-    while city[0] in used_cities:
+    while city[0] in USERS[user_id][2]:
         city = random.sample(cities, 1)
-    print(city[0])
-    used_cities.append(city[0])
     n = Map()
-    return n.draw_map(city[0])
+    return n.draw_map(city[0]), city[0]
 
 
 '''Функция проверяет, есть ли данный пользователь в базе данных, возваращет 
@@ -60,7 +58,6 @@ False, если пользователь не зарегистрирован, Tr
 
 def check_register(user_id):
     result = check_id(user_id)
-    print(result)
     if result:
         res = True
     else:
@@ -68,70 +65,77 @@ def check_register(user_id):
     return res
 
 
-def get_learning_image():
+'''
+Функция возвращает bytes-like object и название объекта, вызывается в режиме обучения
+в функции photo()
+'''
+
+
+def get_learning_image(user_id):
     with open('cities.txt') as fi:
         cities = fi.read()
     cities = cities.replace('.', '')
     cities = cities.split('\n')
     city = random.sample(cities, 1)
-    while city in used_cities:
+    while city in USERS[user_id][2]:
         city = random.sample(cities, 1)
-    used_cities.append(city)
+    USERS[user_id][2].append(city)
     n = Map()
     return n.draw_map(city), city
 
 
-def sights_photo():
+def sights_photo(user_id):
     with open('landmarks.txt') as fi:
         sights = fi.read().split('\n')
     sight = random.sample(sights, 1)
     sight_name = sight[0].split(':')[-1]
     adress = sight[0].split(':')[0]
-    while sight_name in used_cities:
+    while sight_name in USERS[user_id][2]:
         sight = random.sample(sights, 1)
         sight_name = sight[0].split(':')[-1]
         adress = sight[0].split(':')[0]
-    print(sight_name)
-    used_cities.append(sight_name)
+    USERS[user_id][2].append(sight_name)
     n = Map()
-    return n.draw_map(adress)
+    return n.draw_map(adress), sight_name
 
 
 '''Отправка спутникового изображения пользователю'''
 
 
 def photo(peer_id, user_id):
-    global COUNTER, CORRECT, WRONG, LEVEL, LEARNING, SIGHTS
-    if SIGHTS:
-        COUNTER += 1
-        if COUNTER > 15:
-            text = f'Уровень пройден!\nПравильных ответов: {CORRECT} из 15\nВернитесь в главное меню'
+    if USERS[user_id][0] == 'sights':
+        USERS[user_id][5] += 1
+        if USERS[user_id][5] > 15:
+            text = f'Уровень пройден!\nПравильных ответов: {USERS[user_id][3]} из 15\nВернитесь в главное меню'
             vk.messages.send(user_ids=user_id, message=text,
                              random_id=0, keyboard=json.dumps(keyboard_in_game))
             return
-        if COUNTER > 1:
-            msg = f'Правильных ответов &#9989;: {CORRECT}\nНеправильных ответов &#10060;: {WRONG}'
+        if USERS[user_id][5] > 1:
+            msg = f'Правильных ответов &#9989;: {USERS[user_id][3]}\nНеправильных ответов &#10060;: {USERS[user_id][4]}'
             send_message(user_id, msg, keyboard_in_game)
-        msg = f'Вопрос №{COUNTER} из {15}'
+        msg = f'Вопрос №{USERS[user_id][5]} из {15}'
         kb = keyboard_in_game
-        photo = sights_photo()
-    elif LEARNING:
-        photo, city = get_learning_image()
+        photo, s = sights_photo(user_id)
+        USERS[user_id][2].append(s)
+    elif USERS[user_id][0] == 'learning':
+        photo, city = get_learning_image(user_id)
         msg = city
+        USERS[user_id][2].append(city)
         kb = keyboard_learning
     else:
-        COUNTER += 1
-        if COUNTER > 15:
-            text = f'Уровень пройден!\nПравильных ответов: {CORRECT} из 15\nВернитесь в главное меню'
+        USERS[user_id][5] += 1
+        if USERS[user_id][5] > 15:
+            text = f'Уровень пройден!\nПравильных ответов: {USERS[user_id][3]} из 15\nВернитесь в главное меню'
             vk.messages.send(user_ids=user_id, message=text,
                              random_id=0, keyboard=json.dumps(keyboard_in_game))
             return
-        if COUNTER > 1:
-            msg = f'Правильных ответов &#9989;: {CORRECT}\nНеправильных ответов &#10060;: {WRONG}'
+        if USERS[user_id][5] > 1:
+            msg = f'Правильных ответов &#9989;: {USERS[user_id][3]}\nНеправильных ответов &#10060;: {USERS[user_id][4]}'
             send_message(user_id, msg, keyboard_in_game)
-        msg = f'Вопрос №{COUNTER} из {15}'
+        msg = f'Вопрос №{USERS[user_id][5]} из {15}'
         kb = keyboard_in_game
-        photo = get_image(LEVEL)
+        photo, city = get_image(USERS[user_id][1], user_id)
+        USERS[user_id][2].append(city)
     res = vk.photos.getMessagesUploadServer()
     Image.open(photo).save('result.jpg')
     create_board('result.jpg')
@@ -146,25 +150,27 @@ def photo(peer_id, user_id):
     os.remove('result.jpg')
 
 
+'''Отправка пользователю сообщения'''
+
+
 def send_message(us_id, msg, keyboard):
     vk.messages.send(user_id=us_id, message=msg,
                      random_id=random.randint(0, 2 ** 64), keyboard=json.dumps(keyboard))
 
 
+'''Основная функция программы - бот'''
+
+
 def main():
-    global COUNTER, CORRECT, WRONG, LEVEL, LEARNING, SIGHTS
-    TOWN = False
-    RIVER = False
-    WIKI = False
-    LEARNING = False
-    ANSWER = 15
-    WRONG = 0
-    CURRENT_PROMPT = 0
     for event in longpoll.listen():
         if event.type == VkBotEventType.MESSAGE_NEW:
             response = vk.users.get(user_ids=event.obj.message['from_id'])
             text = event.obj.message['text'].lower()
             user_id = event.obj.message['from_id']
+
+            '''Проверка наличия id пользователя в словаре USERS'''
+            if user_id not in USERS.keys():
+                USERS[user_id] = ['', '', [], 0, 0, 0, 0]
 
             '''Проверка регистрации пользователя, в случае, если id пользователя
             отсутствует в базе данных - в БД добаляется id, имя, количество подсказок и время последнего обновления'''
@@ -180,43 +186,34 @@ def main():
             '''Обработка запросов пользователя'''
 
             if text.rsplit(maxsplit=1)[0] == 'выйти в меню':
-                LEVEL = '-'
-                TOWN = False
-                RIVER = False
-                COUNTER = 0
-                CORRECT = 0
-                LEARNING = False
-                WIKI = False
-                WRONG = 0
-                CURRENT_PROMPT = 0
-                used_cities.clear()
+                USERS[user_id] = ['', '', [], 0, 0, 0, 0]
                 send_message(
                     user_id, 'Вы успешно вышли в главное меню!', keyboard_main_menu)
 
-            elif TOWN and LEVEL == '-':
+            elif USERS[user_id][0] == 'town' and USERS[user_id][1] == '':
                 if text == 'лёгкий' or text == 'сложный':
-                    LEVEL = text
+                    USERS[user_id][1] = text
                     photo(event.object.peer_id, user_id)
                 else:
                     send_message(user_id, 'Некорректный ввод!', keyboard_level)
 
-            elif LEARNING and text.rsplit(maxsplit=1)[0] == 'далее':
+            elif USERS[user_id][0] == 'learning' and text.rsplit(maxsplit=1)[0] == 'далее':
                 photo(event.object.peer_id, user_id)
 
-            elif LEARNING and text == 'подробнее':
+            elif USERS[user_id][0] == 'learning' and text == 'подробнее':
                 send_message(user_id, get_summary(
-                    used_cities[-1]), keyboard_learning)
+                    USERS[user_id][2][-1]), keyboard_learning)
 
             elif text == 'достропримечательности':
-                SIGHTS = True
+                USERS[user_id][0] = 'sights'
                 photo(event.object.peer_id, user_id)
 
             elif text.rsplit(maxsplit=1)[0] == 'города':
                 send_message(user_id, 'Выберите сложность: ', keyboard_level)
-                TOWN = True
+                USERS[user_id][0] = 'town'
 
             elif text.rsplit(maxsplit=1)[0] == 'википедия':
-                WIKI = True
+                USERS[user_id][0] = 'wiki'
                 msg = 'Введите объект и мы расскажем Вам о нём! '
                 send_message(user_id, msg, keyboard_wiki)
 
@@ -231,7 +228,7 @@ def main():
                         msg = msg + f'{EM_NUMBERS[j]} ...\n'
                 send_message(user_id, msg, keyboard_main_menu)
 
-            elif WIKI:
+            elif USERS[user_id][0] == 'wiki':
                 msg = get_summary(text)
                 send_message(user_id, msg, keyboard_wiki)
 
@@ -239,36 +236,36 @@ def main():
                 send_message(user_id, 'Выберите режим:', keyboard_modes)
 
             elif text == 'обучение':
-                LEARNING = True
+                USERS[user_id][0] = 'learning'
                 send_message(
                     user_id, 'Добро пожаловать в режим обучения!', keyboard_learning)
                 photo(event.object.peer_id, user_id)
 
             elif text.split()[0] == 'подсказка':
-                length = len(used_cities[-1])
+                length = len(USERS[user_id][2][-1])
                 check_update(user_id)
-                possible = len(used_cities[-1].replace('-', '')) // 3
-                if COUNTER > 15:
+                possible = len(USERS[user_id][2][-1].replace('-', '')) // 3
+                if USERS[user_id][5] > 15:
                     send_message(
                         user_id, 'Уровень пройден!\nВернитесь в главное меню', keyboard_in_game)
-                elif CURRENT_PROMPT + 1 <= possible:
+                elif USERS[user_id][6] + 1 <= possible:
                     ok = use_prompt(user_id)
                     if ok == -1:
                         messag = '&#9940; У вас больше нет подсказок'
                         send_message(user_id, messag, keyboard_in_game)
                     else:
                         messag = f'&#9888; У вас осталось {ok} подсказок'
-                        if CURRENT_PROMPT + 1 <= possible:
-                            CURRENT_PROMPT += 1
-                        msg = f'&#10004; Использована {CURRENT_PROMPT} подсказка из {possible}'
+                        if USERS[user_id][6] + 1 <= possible:
+                            USERS[user_id][6] += 1
+                        msg = f'&#10004; Использована {USERS[user_id][6]} подсказка из {possible}'
                         prompt = []
                         for i in range(length):
-                            if i <= CURRENT_PROMPT - 2:
+                            if i <= USERS[user_id][6] - 2:
                                 prompt.append(
-                                    used_cities[-1][i].upper() + ' ')
-                            elif used_cities[-1][i] == '-':
+                                    USERS[user_id][2][-1][i].upper() + ' ')
+                            elif USERS[user_id][2][-1][i] == '-':
                                 prompt.append(' - ')
-                            elif used_cities[-1][i] == ' ':
+                            elif USERS[user_id][2][-1][i] == ' ':
                                 prompt.append('       ')
                             else:
                                 prompt.append('&#10134;')
@@ -281,21 +278,20 @@ def main():
                                  keyboard_in_game)
 
             elif text.rsplit(maxsplit=1)[0] == 'пропустить':
-                WRONG += 1
-                CURRENT_PROMPT = 0
+                USERS[user_id][4] += 1
+                USERS[user_id][6] = 0
                 photo(event.object.peer_id, user_id)
 
-            elif (TOWN and (LEVEL == 'лёгкий' or LEVEL == 'сложный')) or SIGHTS:
-                print(used_cities[-1])
-                if text == used_cities[-1].lower():
+            elif (USERS[user_id][0] == 'town' and (USERS[user_id][1] == 'лёгкий' or USERS[user_id][1] == 'сложный')) or USERS[user_id][0] == 'sights':
+                if text == USERS[user_id][2][-1].lower():
                     send_message(user_id, f'Верно &#9989;', keyboard_in_game)
-                    CORRECT = CORRECT + 1
-                    CURRENT_PROMPT = 0
+                    USERS[user_id][3] = USERS[user_id][3] + 1
+                    USERS[user_id][6] = 0
                     add_answers(user_id, 1)
                     photo(event.object.peer_id, user_id)
                 else:
-                    WRONG += 1
-                    CURRENT_PROMPT = 0
+                    USERS[user_id][4] += 1
+                    USERS[user_id][6] = 0
                     send_message(user_id, f'Неправильно :(', keyboard_in_game)
                     photo(event.object.peer_id, user_id)
 
@@ -370,7 +366,9 @@ keyboard_in_game = {
     ]
 }
 
-'''Клавиатура, которая активируется в главном меню.'''
+'''
+Клавиатура, которая активируется в главном меню
+'''
 keyboard_main_menu = {
     "one_time": False,
     "buttons": [
@@ -412,6 +410,9 @@ keyboard_main_menu = {
 }
 
 
+'''
+Клавиатура, которая активируется в разделе Википедия
+'''
 keyboard_wiki = {
     "one_time": False,
     "buttons": [
@@ -435,6 +436,10 @@ keyboard_wiki = {
     ]
 }
 
+
+'''
+Клавиатура, которая активируется в разделе режимы
+'''
 keyboard_modes = {
     "one_time": False,
     "buttons": [
@@ -475,6 +480,10 @@ keyboard_modes = {
     ]
 }
 
+
+'''
+Клавиатура, которая активируется в разделе обучения
+'''
 keyboard_learning = {
     "one_time": False,
     "buttons": [
@@ -507,7 +516,6 @@ keyboard_learning = {
     ]
 }
 
-used_cities = []
 
 if __name__ == '__main__':
     main()
